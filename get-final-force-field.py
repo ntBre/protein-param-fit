d1d4d62ac0d9b572fd57121147b044dd4bfa4d40
Chapin Cavender <chapincavender@gmail.com>
2023-03-14 16:28:42 -0700

    Added H bond constraints to final force fields

--- get-final-force-field.py
+++ get-final-force-field.py
@@ -25,7 +25,20 @@ def main(input_ff, output_ff):
     # Load initial force field from ForceBalance with cosmetic attributes
     force_field = ForceField(input_ff, allow_cosmetic_attributes = True)
 
-    # Write force field without cosmetic attributes
+    # Write force field without cosmetic attributes from ForceBalance
+    unconstrained_ff = output_ff.replace('.offxml', '_unconstrained.offxml')
+    force_field.to_file(unconstrained_ff, discard_cosmetic_attributes = True)
+
+    # Add constraints to covalent bonds involving hydrogen
+    constraint_handler = force_field.get_parameter_handler('Constraints')
+    covalent_h_bond_constraint = {
+        'id': 'c1',
+        'smirks': '[#1:1]-[*:2]',
+    }
+    constraint_handler.add_parameter(covalent_h_bond_constraint)
+
+    # Write force field including covalent hydrogen bond constraint without
+    # cosmetic attributes from ForceBalance
     force_field.to_file(output_ff, discard_cosmetic_attributes = True)
 
 
